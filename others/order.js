db2bc6563751e9cf3e306cdce932e00a83233452
Barry1003 <isholausamah@gmail.com>
2023-05-19 11:40:53 +0100

    update

---
+++ others/order.js
@@ -0,0 +1,32 @@
+const orders= [
+    {
+        productName: 'Foldable mini Drones',
+        prodctNumber: '853631',
+        paymentstatus: 'Due',
+        shippings: 'pending',
+    },
+    {
+        productName: 'LAVAENDER KF102 Drones',
+        prodctNumber: '365778',
+        paymentstatus: 'Refunded',
+        shippings: 'Declinded',
+    },
+    {
+        productName: 'RUko fll Drones',
+        prodctNumber: '49347',
+        paymentstatus: 'Due',
+        shippings: 'pending',
+    },
+    {
+        productName: 'Ai camera Drones',
+        prodctNumber: '345631',
+        paymentstatus: 'Due',
+        shippings: 'pending',
+    },
+    {
+        productName: 'GPS 4k Drones',
+        prodctNumber: '853631',
+        paymentstatus: 'paid ',
+        shippings: 'Delivered',
+    },
+];
